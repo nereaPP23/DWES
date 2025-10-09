bffdf7eaa1603763284f6d735242c5c6710f4ec0
Nerea Puertas <neresk27@gmail.com>
2025-10-09 19:32:23 +0200

    ejer4 version2

--- UNIDAD3/practica2_ejer3.php
+++ UNIDAD3/practica2_ejer3.php
@@ -1,9 +1,22 @@
 <?php
 
-$pares = array(2,4,6,8,10,12,14,16,18,20);
+//version 1
+$pares = array(0,2,4,6,8,10,12,14,16,18);
 
 foreach($pares as $valor){
-    echo "$valor<br>";
+    echo "$valor\n";
+}
+
+echo "<br><br>";
+//version 2
+$pares2 = array();
+
+for ($i=0; $i<10; $i++){
+    $pares2[] = $i*2;
+}
+
+foreach($pares2 as $valor){
+    echo "$valor\n";
 }
 
 ?>
