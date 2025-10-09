bcebf31f74424f11e459eecdc67ca511859b7133
Nerea Puertas <neresk27@gmail.com>
2025-10-09 19:29:40 +0200

    ejer2 resuelto

--- UNIDAD3/practica2_ejer2.php
+++ UNIDAD3/practica2_ejer2.php
@@ -1,8 +1,31 @@
 <?php
 
 $alumnos = array(
-    
+    "Basico" => array(
+        'Ingles'=>1,
+        'Frances'=>14,
+        'Aleman'=>8,
+        'Ruso'=>3
+    ),
+    "Medio"=>array(
+        'Ingles'=>6,
+        'Frances'=>19,
+        'Aleman'=>7,
+        'Ruso'=>2
+    ),
+    "Perfeccionamiento"=>array(
+        'Ingles'=>3,
+        'Frances'=>3,
+        'Aleman'=>4,
+        'Ruso'=>1
+    ),
 );
+
+foreach($alumnos as $contenido){
+    foreach($contenido as $indice=>$valor){
+        echo $indice." = ".$valor."<br>";
+    }
+}
 
 
 ?>
