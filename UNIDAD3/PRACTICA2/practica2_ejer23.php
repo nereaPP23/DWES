9b1bbaf08c0d19259cae3af42c56d23921dacc46
Nerea Puertas <neresk27@gmail.com>
2025-10-11 13:44:36 +0200

    ejer23 acabado

--- UNIDAD3/PRACTICA2/practica2_ejer23.php
+++ UNIDAD3/PRACTICA2/practica2_ejer23.php
@@ -14,7 +14,8 @@ $array = array(
 );  
 
 echo "<ul>";
-foreach ($array as $valor) {
+foreach ($array as $familia => $valor) {
+    echo "<br><strong>Familia $familia</strong><br>";
     foreach ($valor as $indice => $valor2) {
         if (is_array($valor2)) {
             echo "<li>$indice:<ul>";
@@ -22,6 +23,44 @@ foreach ($array as $valor) {
                 echo "<li>$valor3</li>";
             }
             echo "</ul></li>";
+        } else {
+            echo "<li>$indice: $valor2</li>";
+        }
+    }
+}
+echo "</ul>";
+
+
+//Opcion 2
+
+/*implode() une los elementos de un array en una sola cadena
+Sintaxis: implode(string $separador, array $array)
+
+Ejemplo:
+
+$array = array('Bart','Lisa','Maggie');
+print implode(',',$array);
+
+Resultado:
+Bart, Lisa, Maggie
+
+$array = array('Chris','Meg','Stewie');
+print implode('-',$array);
+
+Resultado:
+Chris-Meg-Stewie
+
+*/
+
+echo "<h3>Opcion 2 (con implode)</h3>";
+echo "<ul>";
+foreach ($array as $familia => $valor) {
+    echo "<br><strong>Familia $familia</strong><br>";
+    foreach ($valor as $indice => $valor2) {
+        if (is_array($valor2)) {
+            echo "<li>$indice:";
+                echo implode(", ", $valor2);
+            echo "</li>";
         } else {
             echo "<li>$indice: $valor2</li>";
         }
