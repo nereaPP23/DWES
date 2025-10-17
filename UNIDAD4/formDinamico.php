f049ceb91ca134c10942912f67a7988715f9a2a3
Nerea Puertas <nereapp69@educastur.es>
2025-10-17 11:09:45 +0200

    formulario dinamico

--- UNIDAD4/formDinamico.php
+++ UNIDAD4/formDinamico.php
@@ -1,14 +1,29 @@
 
 
+
+
 <html>
     <body>
-        <form action="formDinamico.php" method="post">
-            <?php
-            for ($i = 0; $i <= 10; $i++) {
-                echo "<label for=\"numero$i\">Número $i:</label><br>";
-                echo "<input type=\"number\" id=\"numero$i\" name=\"numero$i\" required><br><br>";
+        <?php
+            if (isset($_POST['enviar'])) {
+                for ($i = 0; $i <= 10; $i++) {
+                echo "El numero $i es: " . $_POST["numero$i"] . "<br>";
+                }
+            } else{
+            echo "<form action=\"formDinamico.php\" method=\"post\">";
+                
+                for ($i = 0; $i <= 10; $i++) {
+                    echo "<label for=\"numero$i\">Número $i:</label><br>";
+                    echo "<input type=\"number\" id=\"numero$i\" name=\"numero$i\" required><br><br>";
+                }
+
+                echo "<button type=\"submit\" name=\"enviar\" value=\"Enviar\">Enviar</button>";
+            echo "</form>";
             }
-            ?>
-            <button type="submit">Enviar</button>
+        ?>
     </body>
 </html>
+
+
+
+
