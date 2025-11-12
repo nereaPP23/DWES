1f001e7cfeb7f471ca7903243fabf047feffc764
Nerea Puertas <neresk27@gmail.com>
2025-11-12 13:17:17 +0100

    insert aciertos

--- UNIDAD4/SESION/JUEGOSIMON/acierto.php
+++ UNIDAD4/SESION/JUEGOSIMON/acierto.php
@@ -2,6 +2,28 @@
 session_start();
 include "pintar-circulos.php";
 
+$hn = 'localhost';
+$db = 'bdsimon';
+$un = 'root';
+$pw = '';
+
+
+
+$connection = new mysqli($hn, $un, $pw, $db);
+ if ($connection->connect_error) die("Fatal Error");
+ $usuario = $_SESSION['usuario'];
+    $query = "SELECT Codigo FROM usuarios WHERE Nombre = '$usuario'";
+    $result = $connection->query($query);
+    if (!$result) die("Fatal Error");
+    $row=$result->fetch_assoc();
+    $codigousu = $row['Codigo'];
+
+    $query2 = "INSERT INTO jugadas (codigousu, acierto) VALUES ($codigousu, 1)";
+        if (!$connection->query($query2)) die("Fatal Error");
+
+        $result->close();
+        $connection->close();
+
     echo "<h2>$_SESSION[usuario], enhorabuena has acertado la combinación!</h2>";
     pintar_circulos(
         $_SESSION['combinacioncorrecta'][0],
