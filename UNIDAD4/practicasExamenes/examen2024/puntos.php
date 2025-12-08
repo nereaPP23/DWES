08840bd765014597d7a3e3c8a1733ef8c2fd974f
Nerea Puertas <neresk27@gmail.com>
2025-12-08 14:40:48 +0100

    examen 2024 hecho

--- UNIDAD4/practicasExamenes/examen2024/puntos.php
+++ UNIDAD4/practicasExamenes/examen2024/puntos.php
@@ -12,6 +12,42 @@ $login = $_SESSION['login'];
 $connection = new mysqli($hn, $un, $pw, $db);
  if ($connection->connect_error) die("Fatal Error");
 
-echo "<h1>Puntos por jugador</h1>"
+echo "<h1>Puntos por jugador</h1>";
 
+
+$query = "SELECT login, puntos FROM jugador";
+$result = $connection->query($query);
+if (!$result) die("Fatal Error");
+
+
+echo <<<_END
+        <table border="1">
+            <tr>
+                <th>Login</th>
+                <th>Puntos</th>
+                <th></th>
+            </tr>
+_END;
+
+
+    $rows = $result->num_rows;
+        for ($j = 0 ; $j < $rows ; ++$j) {
+            $result->data_seek($j);
+            echo '<tr>';
+            echo '<td>' .htmlspecialchars($result->fetch_assoc()['login']). '</td>';
+            $result->data_seek($j);
+            $puntos = $result->fetch_assoc()['puntos'];
+            echo '<td>' .htmlspecialchars($puntos). '</td>';
+            echo "<td>";
+            for ($i = 0; $i < $puntos; $i++) {
+                echo "<div style='width:1px; height:15px;background-color:#00ffeaff; display:inline-block;'></div>";
+            }
+            echo "</td>";
+            echo "</tr>";
+        }
+        
+        $result->close();
+        $connection->close();
+
+        echo "</table><br><br>";
 ?>
--- UNIDAD4/practicasExamenes/examen2024/resultado.php
+++ UNIDAD4/practicasExamenes/examen2024/resultado.php
@@ -13,28 +13,59 @@ $connection = new mysqli($hn, $un, $pw, $db);
 
 $fecha = date("Y-m-d");
 
+//selecionamos la solucion del dia
 $query = "SELECT solucion FROM solucion WHERE fecha = '$fecha'";
 $result = $connection->query($query);
 $solucionDia = "";
 if ($result && $result->num_rows == 1) {
     $fila = $result->fetch_assoc();
     $solucionDia = $fila['solucion'];
+}else{
+    echo "No hay solucion registrada para hoy ($fecha)";
 }
 $result->close();
 
-$queryResp = "SELECT login, hora, respuesta FROM respuestas WHERE fecha = '$fecha'";
-$resultResp = $connection->query($queryResp);
+//suma puntos
+$sumaPuntos=$connection-> query ("UPDATE jugador j SET puntos = puntos + 1 where login in 
+( SELECT login FROM respuestas r, solucion where respuesta = solucion AND r.fecha = '$fecha');");
 
-$aciertos = [];
-$fallos = [];
+//aciertos
+$aciertos = "SELECT login, hora FROM respuestas r
+INNER JOIN solucion s ON r.fecha=s.fecha
+WHERE r.respuesta=s.solucion AND r.fecha='$fecha'";
+$resultA = $connection->query($aciertos);
+
+//fallos
+$fallos = "SELECT login, hora FROM respuestas r
+INNER JOIN solucion s ON r.fecha=s.fecha
+WHERE r.respuesta!=s.solucion AND r.fecha='$fecha'";
+$resultF = $connection->query($fallos);
 
 
-echo <<<_END
-<html>
-    <body>
-        <h1>Fecha: $fecha</h1>
-        <h2>Jugadores acertantes: </h2>
-    </body>
-</html>
-_END;
+
+echo "<html><body>";
+echo "<h1>Fecha: $fecha</h1>";
+
+//cuenta las filas de acertantes con fecha hoy
+echo "<h2>Jugadores acertantes: ".$resultA->num_rows."</h2>";
+echo "<table border='1'>";
+echo "<tr><th>Login</th><th>Hora</th></tr>";
+
+while ($row = $resultA->fetch_assoc()) {
+    echo "<tr><td>".$row['login']."</td><td>".$row['hora']."</td></tr>";
+}
+
+echo "</table>";
+
+//cuenta las filas de fallos con fecha hoy
+echo "<h2>Jugadores que han fallado: ".$resultF->num_rows."</h2>";
+echo "<table border='1'>";
+echo "<tr><th>Login</th><th>Hora</th></tr>";
+
+while ($row = $resultF->fetch_assoc()) {
+    echo "<tr><td>".$row['login']."</td><td>".$row['hora']."</td></tr>";
+}
+echo "</table>";
+
+echo "</body></html>";
 ?>
