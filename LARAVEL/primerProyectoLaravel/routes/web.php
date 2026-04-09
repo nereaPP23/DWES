4c97e86bca6e2b4e4d4dd570173620abb935a123
Nerea Puertas <nereapp69@educastur.es>
2026-04-09 12:38:09 +0200

    rutas

--- LARAVEL/primerProyectoLaravel/routes/web.php
+++ LARAVEL/primerProyectoLaravel/routes/web.php
@@ -7,3 +7,12 @@ Route::get('/', function()
 return '¡Hola mundo!';
 });
 
+Route::get('ruta1/{id?}', function($id=0)
+{
+return 'Esto es la ruta 1 con id '.$id;
+});
+
+Route::get('ruta2', function()
+{
+return 'Esto es la ruta 2';
+});
